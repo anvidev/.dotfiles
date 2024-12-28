017b07fdf17702198dfd9f4d4f15c385edaaf147
anvidev <andreasgylche@gmail.com>
2024-12-28 23:37:47 +0100

    update blink conf

--- nvim/.config/nvim/lua/config/plugins/blink.lua
+++ nvim/.config/nvim/lua/config/plugins/blink.lua
@@ -1,19 +1,17 @@
+---@diagnostic disable: missing-fields
 return {
     {
         'saghen/blink.cmp',
         lazy = false,
         dependencies = {
             'rafamadriz/friendly-snippets',
-            'L3MON4D3/LuaSnip'
+            'L3MON4D3/LuaSnip',
+            "onsails/lspkind.nvim"
         },
-        version = 'v0.*',
+        version = '*',
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
-        ---@diagnostic disable-next-line: missing-fields
         opts = {
-            highlight = {
-                use_nvim_cmp_as_default = true,
-            },
             snippets = {
                 expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
                 active = function(filter)
@@ -28,26 +26,49 @@ return {
                 preset = 'enter',
             },
             completion = {
-                menu = {
-                    draw = {
-                        columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+                accept = {
+                    auto_brackets = {
+                        enabled = true
                     },
                 },
-            },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 200,
+                menu = {
+                    draw = {
+                        columns = { { "kind_icon", "label", gap = 1 }, { "kind" } },
+                    },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 250,
+                    treesitter_highlighting = true,
+                },
             },
             appearance = {
-                nerd_font_variant = 'mono'
+                nerd_font_variant = 'mono',
+                use_nvim_cmp_as_default = true
+            },
+            signature = {
+                enabled = true
             },
             sources = {
                 default = { 'lsp', 'path', 'snippets', 'buffer' },
-                signature = {
-                    enabled = true
+                cmdline = {},
+                providers = {
+                    lsp = {
+                        min_keyword_length = 0,
+                        score_offset = 0,
+                    },
+                    path = {
+                        min_keyword_length = 0,
+                    },
+                    snippets = {
+                        min_keyword_length = 2,
+                    },
+                    buffer = {
+                        min_keyword_length = 4,
+                        max_items = 4,
+                    },
                 },
             },
-            opts_extend = { "sources.default" }
         },
     }
 }
