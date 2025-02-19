8299a4ecc01f29836ba2cbeec061bd78d603e948
anvidev <andreasgylche@gmail.com>
2025-02-19 19:07:57 +0100

    minor changes

--- nvim/.config/nvim/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,8 +1,17 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "tokyonight-night"
-    end
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme "tokyonight-night"
+        end
+    },
+    -- {
+    --     "rose-pine/neovim",
+    --     name = "rose-pine",
+    --     config = function()
+    --         vim.cmd("colorscheme rose-pine")
+    --     end
+    -- }
 }
