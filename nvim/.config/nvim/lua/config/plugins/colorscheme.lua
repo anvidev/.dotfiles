ca1ff54aebe50b977e1b976104d6e879a911c56b
anvidev <andreasgylche@gmail.com>
2025-04-13 10:13:14 +0200

    feat(nvim+ghostty): changed theme to rose-pine

--- nvim/.config/nvim/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,17 +1,23 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "tokyonight-night"
-        end
-    },
     -- {
-    --     "rose-pine/neovim",
-    --     name = "rose-pine",
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
     --     config = function()
-    --         vim.cmd("colorscheme rose-pine")
+    --         vim.cmd.colorscheme "tokyonight-night"
     --     end
-    -- }
+    -- },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                variant = "moon",
+                styles = {
+                    italic = false,
+                }
+            })
+            vim.cmd("colorscheme rose-pine-main")
+        end
+    }
 }
